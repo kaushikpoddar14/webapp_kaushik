16300a4e8e0d3f02ab947a3ea4cc17704fbfa4e3
kaushikpoddar14 <166856471+kaushikpoddar14@users.noreply.github.com>
2025-06-18 10:47:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_app_service_plan" "app_service_plan" {
 }
 
 resource "azurerm_app_service" "app_service" {
-  name                = "app_service_name"
+  name                = "webapp-kaushik"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
